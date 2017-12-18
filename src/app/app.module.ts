e332843a1086d7ec64c91f08f21c2acd6e85c722
chris portscheller <christopher.portscheller@reactivecore.com>
2017-12-17 21:14:42 -0500

    styles(theme): Create first theme commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { ServiceWorkerModule } from '@angular/service-worker';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChrisMaterialModule } from './material.module';
+
 import { AppComponent } from './app.component';
 
 import { environment } from '../environments/environment';
@@ -15,6 +15,7 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    ChrisMaterialModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
---
+++ src/app/material.module.ts
@@ -0,0 +1,69 @@
+import { NgModule } from '@angular/core';
+
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatCheckboxModule,
+  MatInputModule,
+  MatOptionModule,
+  MatSelectModule,
+  MatListModule,
+  MatCardModule,
+  MatChipsModule,
+  MatMenuModule,
+  MatRadioModule,
+  MatDialogModule,
+  MatToolbarModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatIconModule,
+  MatTabsModule,
+  MatSlideToggleModule,
+  MatButtonToggleModule,
+  MatSnackBarModule,
+  MatExpansionModule,
+  MatTableModule,
+  MatSortModule,
+  MatGridListModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatSidenavModule,
+  MatTooltipModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [],
+  exports: [
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatListModule,
+    MatCardModule,
+    MatChipsModule,
+    MatMenuModule,
+    MatRadioModule,
+    MatDialogModule,
+    MatToolbarModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+    MatTabsModule,
+    MatSlideToggleModule,
+    MatButtonToggleModule,
+    MatSnackBarModule,
+    MatTooltipModule,
+    MatExpansionModule,
+    MatTableModule,
+    MatSortModule,
+    MatGridListModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSidenavModule,
+    MatSlideToggleModule
+  ]
+})
+
+export class ChrisMaterialModule { }
